c8c29d9dcb28fa322a99831bcc72afe657725947
Akansha Saxena <akansha.saxena0211@gmail.com>
2022-12-16 03:11:30 +0530

    Adding Base in every page , so that navbar comes in

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
+import Base from "../components/Base";
+
 const Home = () => {
   return (
     <div>
-      <h1>This is home page</h1>
-      <p>Welcome to home page</p>
+      <Base />
     </div>
   );
 };
