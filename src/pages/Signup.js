dd906c9e21f99765c3e00e5d5868233f89f65c50
Akansha Saxena <akansha.saxena0211@gmail.com>
2022-12-16 16:44:36 +0530

    Implemented two way data binding on Signup form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   CardBody,
@@ -15,10 +15,47 @@ import Base from "../components/Base";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Signup = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    about: "",
+  });
+  const [error, setError] = useState({
+    errors: {},
+    isError: false,
+  });
+
+  // handle chnage
+  const handleChange = (event, property) => {
+    // dynamic setting the value
+    setData({ ...data, [property]: event.target.value });
+  };
+
+  // reseting the form
+  const resetData = () => {
+    setData({
+      name: "",
+      email: "",
+      password: "",
+      about: "",
+    });
+  };
+
+  // submit the form
+  const submitForm = (event) => {
+    event.preventDeafult();
+    console.log(data);
+    // data validate
+    // call server api for sending the data
+  };
+
   return (
     <Base>
       <Container>
         <Row className="mt-4">
+          {JSON.stringify(data)}
+
           <Col sm={{ size: 6, offset: 3 }}>
             <Card color="dark" inverse>
               <CardHeader>
@@ -27,11 +64,17 @@ const Signup = () => {
               <CardBody>
                 {/* creating form */}
 
-                <Form>
+                <Form onSubmit={submitForm}>
                   {/* Name field */}
                   <FormGroup>
                     <label for="name">Enter Name</label>
-                    <Input type="text" placeholder="Enter Here..." id="name" />
+                    <Input
+                      type="text"
+                      placeholder="Enter Here..."
+                      id="name"
+                      onChange={(e) => handleChange(e, "name")}
+                      value={data.name}
+                    />
                   </FormGroup>
 
                   {/* email field */}
@@ -41,6 +84,8 @@ const Signup = () => {
                       type="email"
                       placeholder="Enter Here..."
                       id="email"
+                      onChange={(e) => handleChange(e, "email")}
+                      value={data.email}
                     />
                   </FormGroup>
 
@@ -51,17 +96,21 @@ const Signup = () => {
                       type="password"
                       placeholder="Enter Here..."
                       id="password"
+                      onChange={(e) => handleChange(e, "password")}
+                      value={data.password}
                     />
                   </FormGroup>
 
                   {/* Text Area */}
                   <FormGroup>
-                    <label for="about">Enter Password</label>
+                    <label for="about">Enter About</label>
                     <Input
                       type="textarea"
                       placeholder="Enter Here..."
                       id="about"
                       style={{ height: "250px" }}
+                      onChange={(e) => handleChange(e, "about")}
+                      value={data.about}
                     />
                   </FormGroup>
 
@@ -69,7 +118,12 @@ const Signup = () => {
                     <Button outline color="light">
                       Register
                     </Button>
-                    <Button color="secondary" className="ms-2" type="reset">
+                    <Button
+                      onClick={resetData}
+                      color="secondary"
+                      className="ms-2"
+                      type="reset"
+                    >
                       Reset
                     </Button>
                   </Container>
