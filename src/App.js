c8402f72839509fe9a4e61cfb36ca65d009bcea8
Akansha Saxena <akansha.saxena0211@gmail.com>
2022-12-23 01:40:40 +0530

    Center the react-toastify msg

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <BrowserRouter>
-      <ToastContainer />
+      <ToastContainer position="bottom-center" />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
