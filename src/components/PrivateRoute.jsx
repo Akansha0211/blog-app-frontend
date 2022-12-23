9eb896a9786623c49c19a25b3d315700a8d17e54
Akansha Saxena <akansha.saxena0211@gmail.com>
2022-12-23 13:16:19 +0530

    Navigating to Login page if not loggedIn

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
-  return (
-    <div>
-      <h1>PrivateRoute</h1>
-      <Outlet />
-    </div>
-  );
+  let loggedIn = true;
+
+  if (loggedIn) {
+    return <Outlet />;
+  } else {
+    return <Navigate to={"/login"} />;
+  }
+
+  //   return (
+  //     <div>
+  //       <h1>PrivateRoute</h1>
+  //       <Outlet />
+  //     </div>
+  //   );
 };
 
 export default PrivateRoute;
--- src/pages/UserDashBoard.jsx
+++ src/pages/UserDashBoard.jsx
@@ -4,6 +4,10 @@ const UserDashBoard = () => {
   return (
     <div>
       <h1>Welcome to user dashboard</h1>
+      <p>
+        This user page, you hae created very good backend and fronetnd
+        application. work hard to create more even more advanced apps
+      </p>
     </div>
   );
 };
